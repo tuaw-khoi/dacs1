83fc6ec5cb7fd8b5af8a3e370691e95ba3378e31
khoi <lacminhkhoi@gmail.com>
2024-08-17 15:02:44 +0700

    dacs1

---
+++ client/src/main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
+
+window.ipcRenderer.on("main-process-message", (_event, message) => {
+  console.log(message);
+});
